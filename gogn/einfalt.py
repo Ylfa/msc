ca4b62c8d9f8a2201482ee4cd55a64849a289dd8
Ylfa <ylfais@gmail.com>
2017-03-07 10:43:20 +0000

    Writes to SQLite key, area_id, farm_id

--- gogn/einfalt.py
+++ gogn/einfalt.py
@@ -1,32 +1,58 @@
 #Fyrsta tilraun:
 #Þetta les excel skjal og færir gögnin yfir í gagnagrunn
 
+##Skoða næsta skref - Flask
+
+####################### EXCEL #################################
 from openpyxl import load_workbook, worksheet
 #from openpyxl.styles import Font #sjá bold=True
-
+import collections
 wb = load_workbook(filename='Vinnuskjal1.xlsx')
 n = 0
 ws = wb.worksheets[n]
 sheet_names = wb.get_sheet_names()
 
-#Get sheet names
-while len(sheet_names) > 1: #not 0 because last sheet is Spurningar
-    print(sheet_names.pop(0))
-
-#Get farm_name from a sheet. Save empty cells in order to iterate, or save colnr and only names in order to find what cols to iterate
-#print(ws.title) #prints name of sheet
-
-data = [ws.cell(row=1,column=i).value for i in range(ws.min_column,ws.max_column)] #Gets cell values of first row
-print(data)
-
-
+####################### SQLITE  ################################
 import sqlite3 as lite
 import sys
 con = lite.connect('gogn.db')
 print("Database opened successfully.")
 cur = con.cursor()
 db_fields = ['ID', 'AREA_NAME', 'FARM_NAME']
+lykill = 0
+with con:
+    cur.execute('''DROP TABLE IF EXISTS FARM_NAMES''');
+    cur.execute('''CREATE TABLE IF NOT EXISTS FARM_NAMES(
+        ID INTEGER primary key autoincrement,
+        AREA_NAME TEXT,
+        FARM_NAME TEXT);''')
 
+
+
+#Iterate through sheets and get all farm names per sheet
+while len(sheet_names) > 1: #not 0 because last sheet is Spurningar
+    title = ws.title
+    print(title)  # prints name of sheet
+    data = [ws.cell(row=1, column=i).value for i in
+            range(ws.min_column, ws.max_column)]  # Gets cell values of first row
+    #print(data)
+    data_cleaned = [item for item in data if item not in (None, ' ', '  ')] #Remove empty columns
+    print(data_cleaned)
+
+    #Data to be stored in table farm_names: (primary key key,per ws.title, a row for each farmname in data_cleaned)
+    while len(data_cleaned) > 0:
+        data_insert = data_cleaned[0]
+        sql_insert = [title, data_insert]
+        with con:
+            cur.execute('INSERT INTO FARM_NAMES (AREA_NAME, FARM_NAME) VALUES (?,?)', sql_insert)
+        del data_cleaned[0]
+    n = n+1
+    ws = wb.worksheets[n]
+    sheet_names.pop(0) #Popping because there was no function to get_max_sheets in file
+
+
+#data2 = [ws.cell(row=i, column=30).value for i in range(1,20)]
+#print(data2)
 
 #for row in ws.get_squared_range(ws.min_column, ws.min_row, ws.max_column, ws.max_row):
 #    placeholders = []
@@ -38,14 +64,6 @@ db_fields = ['ID', 'AREA_NAME', 'FARM_NAME']
 #    sql = 'INSERT INTO tilraun (' + ','.join(db_fields) + ') VALUES (' + ','.join(placeholders[:len(db_fields)]) + ')'
 #    with con:
 #        cur.execute(sql, tuple(vals[:len(db_fields)])) #make sure only db_fields number of columns
-
-
-with con:
-    cur.execute('''CREATE TABLE IF NOT EXISTS FARM_NAMES(
-        ID INT PRIMARY KEY,
-        AREA_NAME TEXT,
-        FARM_NAME TEXT);''')
-
 
 with con:
     cur.execute("SELECT * FROM farm_names")
