feafbb15eff556ad21ff7d19e44a2cbc9b1e7d77
Ylfa <ylfais@gmail.com>
2017-07-24 15:46:48 +0000

    Chopping block að koma

--- gogn/my-app/einfalt.py
+++ gogn/my-app/einfalt.py
@@ -29,7 +29,7 @@ def init_tables():
 
         cur.execute('''CREATE TABLE IF NOT EXISTS FARM_FAMILY(
           FAMILY_ID INTEGER PRIMARY KEY AUTOINCREMENT,
-          FARM_ID INTEGER,
+          FARM_NAME TEXT,
           FAMILY_YEAR BLOB,
           FAMILY_DATA BLOB);''')
         cur.execute('''CREATE TABLE IF NOT EXISTS FARM_NAMES(
@@ -95,14 +95,16 @@ def init_tables():
     return "Database init done."
 
 
-def get_family(year_data, name_data):
+def get_fams(database_row):
     """
-    Iterate through the list of names in order to group names into familiess.
-    :param year_data: Column of year information
-    :param name_data: Column under name of farm, with names of persons
-    :return: Groupings of families. Store id from farm_names together with groups in db.
+    Iterate through the list of names in order to group names into families.
+    :param row: One row, one farm containing [farm_name, year_data blob, name_data blob]
+
+    :return: [[farm_name, yr1, fam1],[farm_name, yr2, fam2],[farm_name, yr3, fam3]]
+    - Editable web by login: Edit in excel or database?
     """
-    """Popp? count up to next None, Append rows up to None, along with
+
+    """count up to next None, Append rows up to None, along with
     corresponding years as one element in new lists
 
     i = 0
@@ -114,8 +116,40 @@ def get_family(year_data, name_data):
             Write to new lists
     years = year_data
     families = name_data
-    """
-    return years, families
+    print(blob2[1]) --ger första tecknet, ej första elementet. Hitta str efter , som ej ar None, appenda
+    blobs = [blob1, blob2]"""
+
+    return_rows = []
+    n = 0
+    a = database_row[0]
+    b = database_row[1]
+    c = database_row[2]
+    #print(b)
+    #while len(b)>0:
+    d = len(b)
+    #print(d)
+    for i in range(0, len(b)):
+        #print(n)
+        if (b[0] in ('', None, ' ','  ')
+            and c[0] in ('', None, ' ','  ')
+            and n == 0): #báðar fyrstu línur tómar í blobbum, og ekkert fyrir ofan
+            del b[0]
+            del c[0]
+        elif (b[n] in ('', None, ' ','  ')
+              and c[n] in ('', None, ' ','  ')
+              and n > 0): #báðar tómar, gögn í línum fyrir ofan
+            return_rows.append([a, b[:n], c[:n]])
+            a = a[n:] #taka út það sem búið er að bæta við sem fjölla
+            b = b[n:]
+            n = 0 #byrja nýja fjölluskráningu
+        else:
+            n +=1 #gögn á línunni, halda áfram að skrolla niður
+
+
+        #return_rows.append([a, b[items 0-counter], c[items 0-counter]])
+        #del items 0-counter in list b and c
+
+    return return_rows
 
 
 def get_farm_col(list):
@@ -135,6 +169,7 @@ def main():
     wb = load_workbook(filename='Vinnuskjal1.xlsx')
     ws = wb.worksheets[n]
     area_one = []
+    full_list=[]
     sheet_names = wb.get_sheet_names()
 
     a = init_tables()
@@ -146,6 +181,7 @@ def main():
         new_list = get_farm_col(data)
         data_cleaned = [item for item in data if item not in (None, ' ', '  ')]
         area_id = ws.title
+
         with con:
             cur.execute('INSERT INTO AREA_ID (AREA_NAME) VALUES (?)', (sheet_names[0],))
 
@@ -155,10 +191,15 @@ def main():
             year_blob = [ws.cell(row=i, column=b).value for i in range(2, ws.max_row)]
             name_blob = [ws.cell(row=i, column=b+1).value for i in range(2, ws.max_row)]
             sql_insert = [ws.title, id, data_cleaned[0], str(year_blob), str(name_blob)]
-
+            full_list.append([data_cleaned[0], year_blob, name_blob])
+            #while len(year_blob) > 0:
+            #    sql_insert = [ws.title, id, data_cleaned[0], year_blob[0], name_blob[0]]
+            #print(full_list)
             with con:
-                cur.execute('INSERT INTO FARM_NAMES (AREA_NAME, AREA_ID, FARM_NAME, YEAR_DATA, NAME_DATA) VALUES (?, ?, ?,?,?)', sql_insert)
-                # con.commit()
+                cur.execute('INSERT INTO FARM_NAMES (AREA_NAME, AREA_ID, FARM_NAME, YEAR_DATA, NAME_DATA) VALUES (?,?,?,?,?)', sql_insert)
+            #del year_blob[0]
+            #del name_blob[0]
+            # con.commit()
             del data_cleaned[0]
             del new_list[0]
         n += 1
@@ -202,12 +243,35 @@ def main():
         for row in rows:
             cur.execute('INSERT INTO FARM_NAMES7 (AREA_ID, FARM_ID, AREA_NAME,  FARM_NAME, YEAR_DATA, NAME_DATA) VALUES (?,?,?,?,?,?)', row)
 
+    """ Hérna kemur fall sem tekur hvern bæ og klippur niður blobba í fjölskyldutímabil.
+        full_list= nafn bæjar, ár_blob, nafn_blob
+        get_fams: Tekur við bæjarnafn og 2 blobba. Skilar af sér bæjarnafnið og blob-bita flokkaða í tímabil og fjölskyldur
+    """
+    #print(full_list[0])
+
+        #cur.execute("SELECT * FROM FARM_NAMES1")
+        #rows = cur.fetchall()
+        #for row in rows:
+
+    #OBS: BREYTA FAM_NAME Í FAM_ID MEÐ ÞVÍ AÐ JOINA VIÐ AÐRA TÖFLU
+
+    #print(full_list[0])
+    all_fams = get_fams(full_list[0])
+    print(all_fams[0][2])
+    #with con:
+        #for item in all_fams:
+
+            #sql_insert = [ws.title, id, data_cleaned[0], str(year_blob), str(name_blob)]
+
+            #first_fam = [str(all_fams[0][0]), str(all_fams[0][1]), str(all_fams[0][2])]
+            #cur.execute('INSERT INTO FARM_FAMILY (FARM_NAME, FAMILY_YEAR, FAMILY_DATA) VALUES (?,?,?)', first_fam)
+            #del all_fams[0]
 
 main()
 
-"""
+""""
 with con:
-    cur.execute("SELECT * FROM farm_names")
+    cur.execute("SELECT * FROM FARM_NAMES1")
     rows = cur.fetchall()
     for row in rows:
         print(row)
